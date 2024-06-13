e9ecc7f3a912bbe3e9747a4fc3b0fb5810a26c59
emerson <emerson8096@gmail.com>
2024-06-13 18:03:25 -0300

    refactor

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -10,7 +10,7 @@ class UsersController {
     const database = await sqliteConnection()
     const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-    if(checkUserExist){
+    if (checkUserExist) {
       throw new AppError("Este e-mail já está em uso.")
     }
 
@@ -27,33 +27,33 @@ class UsersController {
   }
 
   async update(request, response) {
-    const {name, email, password, old_password} = request.body
+    const { name, email, password, old_password } = request.body
     const user_id = request.user.id
 
     const database = await sqliteConnection()
     const user = await database.get("SELECT * FROM users WHERE id = (?)", [user_id])
 
-    if(!user){
+    if (!user) {
       throw new AppError("Usuário não encontrado")
     }
 
     const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
       throw new AppError("Este e-mail já está em uso")
     }
 
     user.name = name ?? user.name
     user.email = email ?? user.email
 
-    if(password && !old_password){
+    if (password && !old_password) {
       throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
     }
 
-    if(password && old_password){
+    if (password && old_password) {
       const checkOldPassword = await compare(old_password, user.password)
 
-      if(!checkOldPassword){
+      if (!checkOldPassword) {
         throw new AppError("A senha antiga não confere")
       }
 
@@ -71,7 +71,7 @@ class UsersController {
       [user.name, user.email, user.password, now, user_id]
       )
 
-      return response.json()
+    return response.json()
   }
 }
 
